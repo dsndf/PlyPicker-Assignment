1acb2c664f4655d0462f34fd8fb81021bff8d2ad
gaurav jain <gjain8883@gmail.com>
2024-08-31 12:03:11 +0530

    Cors resolved

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const nextConfig = {
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
   reactStrictMode: false,
   env:{
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY ,
